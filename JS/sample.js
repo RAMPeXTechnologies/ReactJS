f1cd48ce8997a93918d8c5c9120e1c99c8a5735c
Rampex <ksprasanthksp10@gmail.com>
2023-08-03 11:31:06 +0530

    Added JS Code

---
+++ JS/sample.js
@@ -0,0 +1,16 @@
+// var a = 120
+// console.log("This is the value declared in var type before altering : " + " " + a)
+// {
+//     var a = 7
+//     console.log("This is the value declared in var type after altering : " + " " + a)
+// }
+// console.log("This is the value declared in var type after block altering : " + " " + a)
+let a = 120
+console.log("This is the value declared in let type before altering : " + " " + a)
+{
+    let a = 7
+    console.log("This is the value declared in let type after altering : " + " " + a)
+}
+console.log("This is the value declared in let type after block altering : " + " " + a)
+const c = 5
+console.log("This is the value declared in const type : " + " " + c)
