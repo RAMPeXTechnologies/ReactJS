553ae71ea464ef0ed3b3f0a4b63a696de4827167
Rampex <ksprasanthksp10@gmail.com>
2023-08-03 16:25:57 +0530

    Integrated JavaScript with HTML

--- JS/JSInter.js
+++ JS/JSInter.js
@@ -45,3 +45,42 @@ if(array.length > 0){
 else{
     console.log("No Elements are in the array")
 }
+
+// Objects
+
+a = {
+    car: "Audi",
+    model: "A8",
+    airbags: 8,
+    wheels: 5,
+    color: "blue"
+}
+
+console.log(a['color'])
+console.log(a.airbags)
+var some = 'model'
+console.log(a[some])
+console.log(a)
+a.car = "BMW"
+console.log(a)
+a.sunroof = {isPresent: "Yes",Shape: "Rectangle"}
+console.log(a)
+console.log(a.sunroof.isPresent)
+for (var ele in a){
+    console.log(ele +":" + a[ele])
+    if(typeof a[ele] === "object")
+    {
+        for (var val in a[ele]){
+            console.log(val +":" + a[ele][val])
+        }
+    }
+}
+
+functions
+
+function fun_name(a,b){
+    console.log(a+ b+5 + 5+ "KSP" + 5 + 5 )
+    return a+ b+5 + 5+ "KSP" + 5 + 5
+}
+
+console.log(fun_name(5,5))
---
+++ JS/linkForHtml.js
@@ -0,0 +1,12 @@
+function myFunfor1st(){
+    document.getElementById("hello1").innerHTML = "I am in my 1st year and studying AI & DS Dept."
+}
+function myFunfor2nd(){
+    document.getElementById("hello1").innerHTML = "I am in my 2nd year and studying AI & DS Dept."
+}
+function myFunfor3rd(){
+    document.getElementById("hello1").innerHTML = "I am in my 3rd year and studying AI & DS Dept."
+}
+function myFunforFinal(){
+    document.getElementById("hello1").innerHTML = "I am in my Final year and studying AI & DS Dept."
+}
